3f411c519479e0e5a9df1d45b5bdc1c0bc896b71
Lilianada <lilibest30.lb@gmail.com>
2023-09-22 16:59:03 +0100

    delete user, search user, sort box, delete bind confurmation modal

--- functions/index.js
+++ functions/index.js
@@ -17,3 +17,38 @@
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+exports.deleteUserAccount = functions.https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+        "unauthenticated",
+        "The function must be called while authenticated.",
+    );
+  }
+
+  const userId = data.userId;
+  if (!userId) {
+    throw new functions.https.HttpsError(
+        "invalid-argument",
+        "Please provide a user ID.",
+    );
+  }
+
+  try {
+    // Delete user from Firebase Auth
+    await admin.auth().deleteUser(userId);
+
+    // Delete user document from Firestore
+    const userDoc = admin.firestore().doc(`user/${userId}`);
+    await userDoc.delete();
+
+    return {success: true, message: "User deleted successfully."};
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    throw new functions.https.HttpsError("unknown", "Failed to delete user.");
+  }
+});
