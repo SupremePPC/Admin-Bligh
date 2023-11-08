51603dc861da14a471e367d71af157f587184e73
Lilianada <lilibest30.lb@gmail.com>
2023-11-08 14:54:45 +0100

    favicon trials and error

--- public/favicon.js
+++ public/favicon.js
@@ -1,8 +1,19 @@
 // favicon.js
-const updateFavicon = (newFaviconUrl) => {
-    const favicon = document.getElementById("favicon");
-    if (favicon) {
-      favicon.href = newFaviconUrl;
+
+const updateFavicon = async () => {
+  const storageRef = ref(storage, "gs://bligh-db.appspot.com/logos/favicon/");
+
+  try {
+    const faviconUrl = await getDownloadURL(storageRef);
+    const faviconElement = document.getElementById("favicon");
+
+    if (faviconElement) {
+      faviconElement.href = faviconUrl;
     }
-  };
-  
+  } catch (error) {
+    console.error("Error updating favicon:", error);
+  }
+};
+
+// Wait for the DOM to fully load before executing the updateFavicon function
+document.addEventListener("DOMContentLoaded", updateFavicon);
