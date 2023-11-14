5f52e513b145823dbf0b3600ab306344ad0928cc
Lilianada <lilibest30.lb@gmail.com>
2023-11-14 17:54:43 +0100

    Updating title too

--- src/components/ChangeMeta/index.jsx
+++ src/components/ChangeMeta/index.jsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from "react";
 import { fetchMetaData, fetchTitleData, updateMetaData, updateTitleText } from "../../firebaseConfig/firestore";
 import Swal from "sweetalert2";
 import "./style.css";
-import { updateMeta } from "../../metaData";
-import { updateMetadata } from "firebase/storage";
+
 
 export default function ChangeMetaData() {
   const [newMeta, setNewMeta] = useState("");
@@ -40,31 +39,39 @@ export default function ChangeMetaData() {
 
   const handleChange = async (e) => {
     e.preventDefault();
-
+  
     try {
       setIsLoading(true);
-
-      await updateMetaData(newMeta);
-      await updateTitleText(newTitle);
-
-      updateMeta(newMeta);
-      updateTitleText(newTitle);
-
+  
+      // Fetch existing data
+      const existingMetaData = await fetchMetaData();
+      const existingTitle = await fetchTitleData();
+  
+      // Use existing values if new values are empty
+      const updatedMetaData = newMeta.trim() !== "" ? newMeta : existingMetaData;
+      const updatedTitle = newTitle.trim() !== "" ? newTitle : existingTitle;
+  
+      // Update meta data and title
+      await updateMetaData(updatedMetaData);
+      await updateTitleText(updatedTitle);
+  
       Swal.fire({
         icon: "success",
-        title: "Image Updated",
-        text: "Your meta data has been updated successfully",
+        title: "Data Updated",
+        text: "Your data has been updated successfully",
         showConfirmButton: false,
         timer: 1500,
       });
+  
+      // Fetch updated data
       fetchExistingMetaData();
       fetchExistingTitleData();
     } catch (error) {
-      console.error("Error updating meta data:", error);
+      console.error("Error updating data:", error);
       Swal.fire({
         icon: "error",
         title: "Error",
-        text: "An error occurred while updating your meta data",
+        text: "An error occurred while updating your data",
         showConfirmButton: false,
         timer: 1500,
       });
@@ -72,12 +79,13 @@ export default function ChangeMetaData() {
       setIsLoading(false);
     }
   };
+  
 
   return (
     <section className="changeMeta_section">
       <div className="section_header">
         <h2 className="title">Change Meta Data and Title</h2>
-        {/* <p className="meta_label">Meta must be at least 8 characters long</p> */}
+        <p className="meta_label">Be sure of your spellings before updating.</p>
       </div>
 
       <form onSubmit={handleChange} className="changeMeta_form">
--- src/metaData.js
+++ src/metaData.js
@@ -1,6 +1,50 @@
-// Assuming you have a function to update meta data in the head of the HTML document
-export function updateMeta(newMetaData) {
-    document.querySelector('meta[name="description"]').setAttribute('content', newMetaData);
-    // You can add similar logic for other meta tags as needed
-  }
-  
+import { useEffect, useState } from "react";
+import { fetchMetaData, fetchTitleData } from "./firebaseConfig/firestore";
+
+const UpdateHeaderData = () => {
+  const [metaData, setMetaData] = useState(""); // Default state is an empty string initially
+  const [pageTitle, setPageTitle] = useState("");
+
+  useEffect(() => {
+    // Fetch initial data from your API
+    const fetchData = async () => {
+      try {
+        const initialMetaData = await fetchMetaData();
+        const initialTitle = await fetchTitleData();
+
+        // Set the default state
+        setMetaData(initialMetaData || "Default Meta Data");
+        setPageTitle(initialTitle || "Default Page Title");
+      } catch (error) {
+        console.error("Error fetching initial data:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // Run only once when the component mounts
+
+  // Function to update the meta data in the HTML head
+  const updateMetaData = () => {
+    const metaElement = document.querySelector("meta[name='description']");
+    if (metaElement) {
+      metaElement.setAttribute("content", metaData);
+    }
+  };
+
+  // Function to update the title in the HTML head
+  const updateTitle = () => {
+    document.title = pageTitle;
+  };
+
+  useEffect(() => {
+    updateMetaData();
+  }, [metaData]);
+
+  useEffect(() => {
+    updateTitle();
+  }, [pageTitle]);
+
+  return null;
+};
+
+export default UpdateHeaderData;
