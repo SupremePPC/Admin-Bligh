e08efa758812d0e73f636954a8d55f07883f4f87
Lilianada <lilibest30.lb@gmail.com>
2023-11-16 16:33:50 +0100

    remove minimum requirement for bonds

--- src/components/BondRequestManagement/Modal/EditBondModal.jsx
+++ src/components/BondRequestManagement/Modal/EditBondModal.jsx
@@ -20,17 +20,17 @@ export default function EditBondModal({
   const [isLoading, setIsLoading] = useState(false);
 
   const handleBuyBonds = async () => {
-    const minimumInvestmentAmount = bond.minimumAmount;
-    if (bondsAmount < minimumInvestmentAmount) {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: `Cannot buy less than $${formatNumber(minimumInvestmentAmount)}`,
-        showConfirmButton: false,
-        timer: 2000,
-      });
-      return;
-    }
+    // const minimumInvestmentAmount = bond.minimumAmount;
+    // if (bondsAmount < minimumInvestmentAmount) {
+    //   Swal.fire({
+    //     icon: "error",
+    //     title: "Oops...",
+    //     text: `Cannot buy less than $${formatNumber(minimumInvestmentAmount)}`,
+    //     showConfirmButton: false,
+    //     timer: 2000,
+    //   });
+    //   return;
+    // }
 
     // Calculate how many bonds the user is buying
     const amountAsNumber = parseFloat(bondsAmount);
